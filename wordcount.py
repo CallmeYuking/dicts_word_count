d1facb71b1e69137306f84ac8dfeca08a097445c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:48:52 -0700

    Implement 'wordcount' function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,20 @@
-# put your code here.
+def wordcount(poem_file):
+    poem = open(poem_file)
+
+    words_count = {}
+
+    for line in poem:
+        line = line.rstrip()
+        line = line.split(' ')
+
+        for word in line:
+            words_count[word] = words_count.get(word, 0) + 1
+
+    for key in words_count:
+        print(key, words_count[key])
+
+    poem.close()
+
+wordcount('twain.txt')
+
+
